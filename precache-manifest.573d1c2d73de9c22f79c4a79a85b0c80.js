bb5530f151a49012316294291480332ab2b82cb6
asis012 <ashish.gyawali@student.ku.edu.np>
2020-01-08 22:41:23 +0545

    Updates

---
+++ precache-manifest.573d1c2d73de9c22f79c4a79a85b0c80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba0f95b88040c805263caa940861aeb",
+    "url": "/app-user/index.html"
+  },
+  {
+    "revision": "cf0148ee07c2e3fcbec5",
+    "url": "/app-user/static/css/main.00ffea14.chunk.css"
+  },
+  {
+    "revision": "0c57002e6eb006f8f193",
+    "url": "/app-user/static/js/2.93692b24.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/app-user/static/js/2.93692b24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf0148ee07c2e3fcbec5",
+    "url": "/app-user/static/js/main.913ec95f.chunk.js"
+  },
+  {
+    "revision": "eed1c94489126961e970",
+    "url": "/app-user/static/js/runtime-main.71f8b064.js"
+  }
+]);
